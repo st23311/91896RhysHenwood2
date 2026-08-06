5cb993be1007f4c10efeb1c7508a3ce5507d7576
st23311 <st23311@howick.school.nz>
2026-08-06 13:22:42 +1200

    Old reposatory was not working, but added Functions for licence number

---
+++ 91896RhysHenwoodv1.py
@@ -0,0 +1,35 @@
+# List of menu options
+menu = [
+    "1. Record a speeding offence",
+    "2. View all recorded offences",
+    "3. Search offence records",
+    "4. Display patrol summary",
+    "5. Exit Program"
+]
+
+# Dictionary for menu choices
+menu_options = {
+    "1": "Record a speeding offence",
+    "2": "View all recorded offences",
+    "3": "Search offence records",
+    "4": "Display patrol summary",
+    "5": "Exit Program"
+}
+
+#function to display menu.
+def display_menu():
+    print("\n===== Speeding Offence System =====")
+    for option in menu:
+        print(option)
+
+# Main Program
+while True:
+    display_menu()
+    choice=input("\nEnter your choice (1-5): ")
+    
+    if choice in menu_options:
+        if choice == "5":
+            print("exiting program...")
+            break
+        else:
+            print("Invalid choice. Pleas enter a number between 1 and 5.")
